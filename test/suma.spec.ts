06476c863f7a3a4bf7f5737460c5368f5187a7ba
langelxobl <langelxobl@gmail.com>
2023-07-12 18:37:21 -0500

    test example

--- test/suma.spec.ts
+++ test/suma.spec.ts
@@ -1,7 +1,9 @@
+import request from 'supertest';
 import app from '../src/config/app';
 
-describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+describe('welcome test', () => {
+  test('api running', async () => {
+    const res = await request(app).get('/').send();
+    expect(res.statusCode).toBe(200);
   });
 });
